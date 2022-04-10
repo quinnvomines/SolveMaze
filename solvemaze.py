7a6d4c539dfc8f8505a3293838dd85ad13eb3993
Quinn <quinnvo@mines.edu>
2022-04-09 21:54:41 -0600

    Fixed a bug

--- solvemaze.py
+++ solvemaze.py
@@ -1,5 +1,5 @@
-str_input = 'FFFFRRFFLFLFRRFFFLFFFLFLFFRFFFLFRRFFRFLFLF'
-expected_output = 'FFRFRFFLFFFLFLFFRFFFRFRFLFLF'
+str_input = 'FLFLFRFRFFRRFFLFLFRFRFLFLFFLFFRFLFRFRFRRFLFLFFFLFLF'
+expected_output = 'RFLFFLFFRFRFFLFLF'
 input = list(str_input)
 
 def countForwardMoves(lst):
@@ -10,6 +10,10 @@ def countForwardMoves(lst):
         else:
             break
     return count
+
+def removeN():
+    global input
+    input[:] = [x for x in input if x != 'N']
 
 def replace_pattern1(index):
     global input
@@ -43,33 +47,36 @@ def replace_pattern4(index):
         input[index] = 'L'
         input[index+1] = 'L'
 
-for i in range(len(input)):
-    if(i + 1 < len(input) and input[i] == 'R' and input[i + 1] == 'R'):
-        #Count number of forwards before and after 180 turn
-        count_before180 = countForwardMoves(input[0:i][::-1])
-        count_after180 = countForwardMoves(input[(i+2):])
-        num_forwd_deadend = min([count_before180, count_after180])
+while(('').join(input).find('RR') != -1):
+    for i in range(len(input)):
+        if(i + 1 < len(input) and input[i] == 'R' and input[i + 1] == 'R'):
+            #Count number of forwards before and after 180 turn
+            count_before180 = countForwardMoves(input[0:i][::-1])
+            count_after180 = countForwardMoves(input[(i+2):])
+            num_forwd_deadend = min([count_before180, count_after180])
 
-        #Stop if no forward moves after 180 turn
-        if(num_forwd_deadend == 0):
-            continue
+            #Stop if no forward moves after 180 turn
+            if(num_forwd_deadend == 0):
+                continue
 
-        #Check pattern 1 and replace
-        if(i-num_forwd_deadend-1 >= 0 and i+num_forwd_deadend+3 < len(input)):
-            replace_pattern1(i)
+            #Check pattern 1 and replace
+            if(i-num_forwd_deadend-1 >= 0 and i+num_forwd_deadend+3 < len(input)):
+                replace_pattern1(i)
 
-        #Check pattern 2 and replace
-        if(i-num_forwd_deadend-1 >= 0):
-            replace_pattern2(i)
+            #Check pattern 2 and replace
+            if(i-num_forwd_deadend-1 >= 0):
+                replace_pattern2(i)
 
-        #Check pattern 3 and replace
-        if(i+num_forwd_deadend+2 < len(input)):
-            replace_pattern3(i)
+            #Check pattern 3 and replace
+            if(i+num_forwd_deadend+2 < len(input)):
+                replace_pattern3(i)
 
-        #Check pattern 4 and replace
-        replace_pattern4(i)
+            #Check pattern 4 and replace
+            replace_pattern4(i)
+    removeN()
 
 print("input: \t\t\t\t" + str_input)
+print("output: \t\t\t" + ('').join(input))
 print("output: \t\t\t" + ('').join(input).replace('N',''))
 print("expected_output: \t" + expected_output)
 if(('').join(input).replace('N','') == expected_output):
